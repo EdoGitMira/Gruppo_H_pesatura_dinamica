642d4df277adf457ef8988018d9b805c2fd92bde
EdoGitMira <e.mirandola@studenti.unibs.it>
2021-11-06 12:58:50 +0100

    aggiunta urlname masse di prova

--- ScriptGestioneDatiFeatures/fileurl.py
+++ ScriptGestioneDatiFeatures/fileurl.py
@@ -5,10 +5,19 @@ from urllib import request
 def somma(a,b):
     return a+b
 
-def readTxtUrl(url):
-    path = Path(__file__).parent / "../dati/taratura"  # path relativa quindi torna indietro e poi va avanti nelle cartelle definite.
+def readTxtUrl(url,urlnamemass):
+    ''' funzione per la lettura di file .TXT da file .url di github in con link RAW'''
+
+    path = Path(
+        __file__).parent / "../dati/taratura"  # path relativa quindi torna indietro e poi va avanti nelle cartelle definite.
     lista = listdir(path)
     data = []
+
+    #listaname  = []
+    #for line in request.urlopen(urlnamemass):
+    #    listaname.append(line)
+
+    #for name in listaname:
     for name in lista:
         urlc = url + name
         print('-' * 100)
