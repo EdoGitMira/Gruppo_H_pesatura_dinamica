dfcf822229d21c72477006a2f453016ccb10789f
99Abdel <a.msaad@studenti.unibs.it>
2021-11-18 10:31:02 +0100

    dd

--- ScriptGestioneFeatures/dynamic_data_work/letturaFileDinanico.py
+++ ScriptGestioneFeatures/dynamic_data_work/letturaFileDinanico.py
@@ -1,5 +1,11 @@
+import urllib
+from urllib import request
 
-from static_data_work.PreparazioneFile import *
+def read_names_url_txt(url):
+    names = []
+    for element in urllib.request.urlopen(url):
+        names.append(element.decode('utf-8').strip('\n'))
+    return names
 
 
 def lista_features_dinamico(url_repo, url_name):  # read all file from git hub and calculate the feature static
@@ -33,7 +39,7 @@ def creazione_numeri_dinamici(file, mass_name):
     masses_filt = []
     data = [0,0,0,0,0]
     for (i, element) in enumerate(file):
-        if i > 5:
+        if i > 4:
             element = element.decode('utf-8')
             for index in range(1,5):
                 data[index] = crea_numero(element,index)
@@ -50,6 +56,6 @@ def creazione_numeri_dinamici(file, mass_name):
 def crea_numero(element,i):
     bridge = element.split('\t')[i].split('\n')[0].split('E')
     base = float(bridge[0].replace(',', '.'))  # number without exponent
-    exponent = int(bridge[i])  # exponent of number
+    exponent = int(bridge[1])  # exponent of number
     number = round(base * pow(10, exponent), 10)
     return number
