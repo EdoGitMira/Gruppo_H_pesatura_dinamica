6727f7dae8e6c3e0880a9c4bbcb5b8ee2d21cd96
99Abdel <a.msaad@studenti.unibs.it>
2021-11-25 13:11:29 +0100

    media mobile

--- ScriptGestioneFeatures/static_data_work/testmisura.py
+++ ScriptGestioneFeatures/static_data_work/testmisura.py
@@ -1,8 +1,10 @@
-from PreparazioneFile import *
+from CalcoloFeatures import *
 from math import *
+import pandas as pd
 from matplotlib import pyplot as plt
+import numpy as np
 
-nome = '20g.txt'
+
 url = 'https://raw.githubusercontent.com/EdoGitMira/Progetto_Laboratorio_Misure_pesatura_dinamica/main/dati'
 url_name_velocity = 'https://raw.githubusercontent.com/EdoGitMira/Progetto_Laboratorio_Misure_pesatura_dinamica/main/dati/speed.txt'
 
@@ -12,14 +14,20 @@ def medieTemporalei(voltage, n):
     list_div = [mean(voltage[i * n:(i + 1) * n]) for i in range((len(voltage) + n - 1) // n)]
     return list_div
 
+f = pd.read_csv('348.06-25-11-11-59-09.csv',delimiter=';')
+f['Bridge'] = [x.replace(',', '.') for x in f['Bridge']]
 
-#[lista,grams,voltage] = lista_features_statico(url,url_name_velocity)
+voltage = f.Bridge.to_list()
+voltage = voltage[3:]
+voltage = np.float_(voltage)
+N = 60000
+lista_medie = np.convolve(voltage, np.ones(N)/N, mode='valid')
 
-voltage = [uniform(0.000986,0.000988) for i in range(20000)]
-lista_medie = medieTemporalei(voltage,2000)
+#lista_medie = medieTemporalei(voltage,n)
 
 plt.plot(lista_medie)
 plt.show()
+plt.title('media mobile')
 
 
 
