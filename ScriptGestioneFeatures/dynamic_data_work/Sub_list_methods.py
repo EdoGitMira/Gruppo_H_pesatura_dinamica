69b632c133340c949930bff7171c15b9b33dac35
99Abdel <a.msaad@studenti.unibs.it>
2021-11-29 22:27:36 +0100

    riprendere il dinamico

--- ScriptGestioneFeatures/dynamic_data_work/Sub_list_methods.py
+++ ScriptGestioneFeatures/dynamic_data_work/Sub_list_methods.py
@@ -1,6 +1,14 @@
 import numpy as np
+from statistics import *
+def split_list(lista):
+    '''separa la lista passata in 3 liste:\n
+    - lista tra fronte di salita FTC1 e discesa FTC2
+    - lista tra fronte di discesa FT2 e salita FT1
+    - lista tra frnte di discesa FTC1 e di discesa FTC2
 
-def centrale_grande(lista):
+    :returns: lista_centrale_grande, lista_centrale_piccola, lista_no_dim
+   '''
+
     i = 0
     fronte_salita_FC1 = 0
     fronte_discesa_FC1 = 10
@@ -27,5 +35,14 @@ def centrale_grande(lista):
     lista_centrale_grande = [lista[1][fronte_salita_FC1:fronte_discesa_FC2],lista[4][fronte_salita_FC1:fronte_discesa_FC2]]
     lista_centrale_piccola = [lista[1][fronte_discesa_FC1:fronte_salita_FC2],lista[4][fronte_discesa_FC1:fronte_salita_FC2]]
     lista_no_dim = [lista[1][fronte_discesa_FC1:fronte_discesa_FC2],lista[4][fronte_discesa_FC1:fronte_discesa_FC2]]
+    salita = [lista[1][:fronte_salita_FC1],lista[4][:fronte_salita_FC1]]
+    discesa = [lista[1][fronte_discesa_FC2:],lista[4][fronte_discesa_FC2:]]
+    return lista_centrale_grande, lista_centrale_piccola, lista_no_dim, salita, discesa
 
-    return lista_centrale_grande, lista_centrale_piccola, lista_no_dim
+def tratto_costante(dati):
+    '''prende i dati e considera solo il tratto 'costante' che indicativamente è gli ultimi 3/4 del vettore'''
+    da = len(dati)/4
+    dati = dati[da:]
+    media = mean(dati)
+    std_dev = stdev(dati)
+    return media,std_dev
